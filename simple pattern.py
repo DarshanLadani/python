9783dd333e3eee67a96fda88833db3c06c56f73c
DarshanLadani <118542660+DarshanLadani@users.noreply.github.com>
2023-04-22 18:00:19 +0530

    Add files via upload

---
+++ simple pattern.py
@@ -0,0 +1,9 @@
+for i in range(10):
+    print("*"*i)
+
+for i in range(10):
+    print(" "*(9-i),"*"*i)
+for i in range(10):
+    print(" "*(9-i)," *"*i)
+for i in range(8,0,-1):
+    print(" "*(9-i)," *"*i)
